3d11434a2659a7a8c37a69ba8d68a38f72b0e84d
Nubgle <32046884+Nubgle@users.noreply.github.com>
2018-03-24 14:38:20 -0600

    Create github.js

---
+++ github.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client();
+
+Client.on("ready", () => {
+  console.log("Inxhinieri Prototype Active");
+
+Client.on("message", (message) => {
+});
+
+Client.login("NDI3MjAxNjA0ODE5NzQ2ODE2.DZhG-Q.-T5jAs7s-977RWn97kBmkr5oASQ");
